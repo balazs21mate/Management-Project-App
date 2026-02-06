3ba6596b2d880c4cc5978a6acdc5abefc50af015
Máté Balázs <balazs21mate@gmail.com>
2026-02-06 16:04:06 +0100

    Project és Section pozíció átállítása lg:mt-vel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 export default function Projects({ onNewProject }) {
   return (
-    <section className="w-1/5 bg-black  mt-20 rounded-tr-3xl pl-14 pt-32">
+    <section className="w-1/5 bg-black  lg:mt-20 rounded-tr-3xl pl-14 pt-32">
       <h2 className="text-white uppercase text-3xl font-bold">Your Projects</h2>
       <button
         onClick={onNewProject}
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ children }) {
   return (
-    <section className="flex flex-col justify-center items-center w-4/5 bg-transparent">
+    <section className="flex flex-col lg:mt-40 items-center w-4/5 bg-transparent">
       {children}
     </section>
   );
