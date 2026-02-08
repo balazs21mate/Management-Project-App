0360de089d92a21d3083f36a342a431d48e12ce5
balazs21mate <balazs21mate@gmail.com>
2026-02-08 17:01:05 +0100

    NewProjectInput forwardRef-el átalakítása és ref-ek létrehozása az AddNewProject komponensben.

--- src/components/AddNewProject.jsx
+++ src/components/AddNewProject.jsx
@@ -1,24 +1,38 @@
 import NewProjectInput from "./NewProjectInput";
+import { useRef } from "react";
 
 export default function AddNewProject({ onExitNewProject }) {
+  const title = useRef();
+  const description = useRef();
+  const date = useRef();
+
+  function handleRefs() {
+    console.log(title.current.value);
+    console.log(description.current.value);
+    console.log(date.current.value);
+
+    onExitNewProject();
+  }
+
   return (
     <>
       <div className="self-end mr-60">
         <button onClick={onExitNewProject}>Cancel</button>
         <button
-          onClick={onExitNewProject}
+          onClick={handleRefs}
           className="bg-black text-white rounded-lg py-2 px-6 ml-6"
         >
           Save
         </button>
       </div>
-      <NewProjectInput inputType="text" labelText="title" />
+      <NewProjectInput ref={title} inputType="text" labelText="title" />
       <NewProjectInput
+        ref={description}
         inputType="text"
         labelText="description"
         isTextArea={true}
       />
-      <NewProjectInput inputType="date" labelText="due date" />
+      <NewProjectInput ref={date} inputType="date" labelText="due date" />
     </>
   );
 }
--- src/components/NewProjectInput.jsx
+++ src/components/NewProjectInput.jsx
@@ -1,19 +1,27 @@
-export default function NewProjectInput({ inputType, labelText, isTextArea }) {
-  return (
-    <div className="flex flex-col w-full text-stone-600 my-3">
-      <label className="uppercase ml-20 mb-1 font-bold">{labelText}</label>
-      {isTextArea ? (
-        <textarea
-          name="input"
-          className="bg-stone-300 border-b-2 border-stone-400 hover:border-stone-600 mr-60 focus:border-stone-600 focus:outline-none ml-20 px-2 py-3"
-        />
-      ) : (
-        <input
-          type={inputType}
-          name="input"
-          className="bg-stone-300 border-b-2 border-stone-400 hover:border-stone-600 mr-60 focus:border-stone-600 focus:outline-none ml-20 px-2 py-3"
-        />
-      )}
-    </div>
-  );
-}
+import { forwardRef } from "react";
+
+const NewProjectInput = forwardRef(
+  ({ inputType, labelText, isTextArea }, ref) => {
+    return (
+      <div className="flex flex-col w-full text-stone-600 my-3">
+        <label className="uppercase ml-20 mb-1 font-bold">{labelText}</label>
+        {isTextArea ? (
+          <textarea
+            ref={ref}
+            name="input"
+            className="bg-stone-300 border-b-2 border-stone-400 hover:border-stone-600 mr-60 focus:border-stone-600 focus:outline-none ml-20 px-2 py-3"
+          />
+        ) : (
+          <input
+            ref={ref}
+            type={inputType}
+            name="input"
+            className="bg-stone-300 border-b-2 border-stone-400 hover:border-stone-600 mr-60 focus:border-stone-600 focus:outline-none ml-20 px-2 py-3"
+          />
+        )}
+      </div>
+    );
+  },
+);
+
+export default NewProjectInput;
