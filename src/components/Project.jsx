186562afbab5cde95f575962a3a974440e6c03f6
Máté Balázs <balazs21mate@gmail.com>
2026-02-19 23:48:54 +0100

    Dátum formázása

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,23 @@
 export default function Project({ title, description, date }) {
+  function formatDate(date) {
+    return new Date(date).toLocaleDateString("en-Us", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+  }
   return (
     <>
       <div className="w-full flex justify-between">
-        <h1>{title}</h1>
-        <button>delete</button>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <button>Delete</button>
       </div>
-      <p>{description}</p>
+      <time className="self-start text-stone-400 mt-3 mb-4">
+        {formatDate(date)}
+      </time>
+      <p className="self-start border-b-4 border-stone-200 pb-4 w-full">
+        {description}
+      </p>
     </>
   );
 }
