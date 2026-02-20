511c5b221ace62bcd164dda54096b5e29ba18fb7
Máté Balázs <balazs21mate@gmail.com>
2026-02-21 00:30:47 +0100

    TaskLog kialakítása

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,8 @@
 export default function Task({ task }) {
   return (
-    <>
+    <div className="flex justify-between my-6">
       <p>{task}</p>
       <button>Clear</button>
-    </>
+    </div>
   );
 }
--- src/components/TaskLog.jsx
+++ src/components/TaskLog.jsx
@@ -1,5 +1,11 @@
 import Task from "./Task";
 
 export default function TaskLog({ tasks }) {
-  return <div className="flex w-full px-4 py-8 mt-10 bg-stone-100"></div>;
+  return (
+    <div className="w-full justify-between px-4 py-8 mt-10 bg-stone-100">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task.task} />
+      ))}
+    </div>
+  );
 }
